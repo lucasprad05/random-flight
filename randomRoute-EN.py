d2c48066002f7fb4587d438d527985268671ff5f
Lucas Prado <88468707+lucasprad05@users.noreply.github.com>
2022-09-27 23:06:58 -0300

    Add files via upload

---
+++ randomRoute-EN.py
@@ -0,0 +1,28 @@
+#Random Flight Route Ideas
+#by Lucas Prado
+
+import random
+
+def randomRoute(list):
+    N = len(list) - 1
+    apt1 = random.randint(0, N)
+    apt2 = random.randint(0, N)
+    if apt1 == apt2:
+        return randomRoute(list)
+    print('Route: ' + list[apt1] + ' - ' + list[apt2])
+    op = input('Do you wanna sort again? (y/n): ')
+    if op == 'y' or op == 'Y':
+        randomRoute(list)
+
+def main():  
+    list = []
+    j = 0
+    while j == 0:
+        x = input('Type an ICAO airport or "e" to exit: ')
+        if x != 'e' and x != 'E':
+            list.append(x)
+        else:
+            j = 1
+    randomRoute(list)
+
+main()
